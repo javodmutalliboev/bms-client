b830beb17311880384f1cb2b109f07b150f25661
Javod Mutalliboev <javodmutalliboev@gmail.com>
2023-11-11 18:59:11 +0500

    signup get existing account list integrated

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css'],
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
   onSubmit(form: NgForm) {
     console.log(form);
   }
 
-  email = /^[\w\d]+@[\w\d]+\.[\w\d]+$/;
+  pattern = /^[\w\d]+@[\w\d]+\.[\w\d]+$/;
+
+  constructor(
+    private httpClient: HttpClient,
+    private toastService: ToastrService
+  ) {}
+
+  euList: string[] = [];
+
+  emailExists(email: string) {
+    return this.euList.find((e) => e === email);
+  }
+
+  gettingEuList = false;
+  ngOnInit(): void {
+    this.gettingEuList = true;
+    this.httpClient.get<string[]>(`${environment.apiUrl}/euList`).subscribe({
+      next: (list) => {
+        this.gettingEuList = false;
+        this.euList = list;
+      },
+      error: (err) => {
+        this.gettingEuList = false;
+        this.toastService.error('An error occurred', '', {
+          disableTimeOut: true,
+        });
+      },
+    });
+  }
 }
