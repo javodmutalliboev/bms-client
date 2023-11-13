0bc3b423e3ecdc6f9d0be8068c6224e79a146752
Javod Mutalliboev <javodmutalliboev@gmail.com>
2023-11-13 19:33:50 +0500

    login some update

--- src/app/components/collections/collections.component.ts
+++ src/app/components/collections/collections.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-collections',
   templateUrl: './collections.component.html',
-  styleUrls: ['./collections.component.css']
+  styleUrls: ['./collections.component.css'],
 })
-export class CollectionsComponent {
+export class CollectionsComponent implements OnInit {
+  constructor(private cookieService: CookieService) {}
 
+  ngOnInit(): void {
+    console.log(JSON.parse(this.cookieService.get('user')));
+  }
 }
