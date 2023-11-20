8c7bd2bde95f298b42c2ccbf5ecad8c3243ee59b
Javod Mutalliboev <javodmutalliboev@gmail.com>
2023-11-20 21:27:17 +0500

    in progress

--- src/app/administrator/content/components/users/users.component.ts
+++ src/app/administrator/content/components/users/users.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  constructor(private httpClient: HttpClient) {}
 
+  ngOnInit(): void {
+    this.httpClient
+      .get(`${environment.apiUrl}/administrator/users`)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
