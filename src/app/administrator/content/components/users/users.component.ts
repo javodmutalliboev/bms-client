5314aa2eb1da56cb0d8368d8e57f2ddf10a62231
Javod Mutalliboev <javodmutalliboev@gmail.com>
2023-11-21 12:49:52 +0500

    in progress

--- src/app/administrator/content/components/users/users.component.ts
+++ src/app/administrator/content/components/users/users.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -8,13 +9,26 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.httpClient
-      .get(`${environment.apiUrl}/administrator/users`)
-      .subscribe((response) => {
-        console.log(response);
+      .get(`${environment.apiUrl}/administrator/users`, {
+        withCredentials: true,
+      })
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.log(error);
+          this.toastrService.error('An error occurred', '', {
+            disableTimeOut: true,
+          });
+        },
       });
   }
 }
