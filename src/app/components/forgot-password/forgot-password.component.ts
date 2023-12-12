c550f0bded373a36a9fffcc69c9e0e3aac57d72e
Javod Mutalliboev <javodmutalliboev@gmail.com>
2023-12-12 11:48:10 +0500

    in progress

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css'],
 })
-export class ForgotPasswordComponent {}
+export class ForgotPasswordComponent {
+  pattern = /^[\w\d]+@[\w\d]+\.[\w\d]+$/;
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+  }
+}
